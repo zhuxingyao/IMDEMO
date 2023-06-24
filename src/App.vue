e314d0f2233cd64e76faf455eece899a43a01d46
祝星耀 <zhuxingyao1@rongcloud.cn>
2023-06-24 19:23:21 +0800

    feat: 完善交互逻辑

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
 import { onBeforeMount, ref, nextTick } from 'vue'
-import { PictureFilled, Promotion } from '@element-plus/icons-vue'
+import { PictureFilled, Promotion, Delete, MoreFilled } from '@element-plus/icons-vue'
 import { ElNotification, ElLoading, ElScrollbar } from 'element-plus'
 import { 
   Events,
@@ -14,6 +14,8 @@ import {
   IConversationOption,
   GetHistoryMessageOption,
   GetHistoryMessageResult,
+  IExpansionListenerData,
+  IReadReceiptReceivedEvent,
 
   init,
   addEventListener,
@@ -24,6 +26,12 @@ import {
   getHistoryMessages,
   sendMessage,
   TextMessage,
+  deleteMessages,
+  recallMessage,
+  updateMessageExpansion,
+  sendReadReceiptMessage,
+  getChatroomHistoryMessages,
+joinChatRoom,
 } from '@rongcloud/imlib-next'
 
 const activeIndex = ref('1');
@@ -33,6 +41,10 @@ let loading: any;
 
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
+  activeIndex.value = key
+  currentChatroom.value = {}
+  currentConversation.value = undefined
+  localMessageList.value = []
 }
 
 const cardCss = {
@@ -46,7 +58,19 @@ const localConversationList = ref<IAReceivedConversation[]>()
 const currentConversation = ref<IAReceivedConversation>()
 // 本地 UI 历史消息渲染数据
 const localMessageList = ref<IAReceivedMessage[]>()
-
+// 当前登入的用户 id
+const currentUserId = ref<string>()
+// 当前会话已读时间
+const currentUnreadInfo = ref<any>({
+  messageUId: '',
+  sentTime: 1,
+})
+// 本地聊天室列表
+const localChatroomList = ref([
+  { targetId: 'Chatroom01', name: '西游记' },
+  { targetId: 'Chatroom02', name: '葫芦娃救爷爷' }
+])
+const currentChatroom = ref()
 
 onBeforeMount(async () => {
   /**
@@ -69,6 +93,9 @@ onBeforeMount(async () => {
       return
     }
     evt.conversationList.forEach((item) => {
+      if (!localConversationList.value) {
+        return
+      }
       const index: number = localConversationList.value!.findIndex((con) => {
         return con.targetId === item.conversation.targetId && con.conversationType === item.conversation.conversationType && con.channelId === item.conversation.channelId
       })
@@ -77,17 +104,52 @@ onBeforeMount(async () => {
         return
       }
       localConversationList.value![index] = item.conversation
+      
     })
 
   })
   addEventListener(Events.CHATROOM, (evt: IChatroomListenerData) => {
     console.log('IChatroomListenerData', evt)
   })
+  addEventListener(Events.EXPANSION, (evt: IExpansionListenerData) => {
+    console.log('IExpansionListenerData', evt)
+    if (!localMessageList.value) {
+      return
+    }
+    const index: number = localMessageList.value!.findIndex((item) => item.messageUId === evt.updatedExpansion?.messageUId);
+    if (index < 0) {
+      return
+    }
+    localMessageList.value![index].expansion = evt.updatedExpansion?.expansion;
+  })
+  addEventListener(Events.READ_RECEIPT_RECEIVED, (evt: IReadReceiptReceivedEvent) => {
+    console.log('IReadReceiptReceivedEvent', evt)
+    // ${evt.conversation.conversationType}-${evt.conversation.targetId}-
+    const key = `${(window as any).APPKEY}-${currentUserId.value}-RECEIVED`
+    const data = localStorage.getItem(key);
+    if (data) {
+      const newData = { ...JSON.parse(data), [`${evt.conversation.conversationType}-${evt.conversation.targetId}`]: {
+        messageUId: evt.messageUId,
+        sentTime: evt.sentTime,
+      }}
+      localStorage.setItem(key, JSON.stringify(newData));
+    }
+    localStorage.setItem(key, JSON.stringify({
+      [`${evt.conversation.conversationType}-${evt.conversation.targetId}`]: {
+        messageUId: evt.messageUId,
+        sentTime: evt.sentTime,
+      }
+    }));
+    if (evt.conversation.targetId === currentConversation.value?.targetId && evt.conversation.conversationType === currentConversation.value?.conversationType) {
+      currentUnreadInfo.value = evt
+    }
+  })
   addEventListener(Events.PULL_OFFLINE_MESSAGE_FINISHED, async () => {
     pullOfflineMessageFinished.value = true;
     // 关闭遮罩层
     loading.close();
-    const { code, data } = await getConversationListHandle()
+    const { code, data } = await getConversationListHandle();
+    console.log('data', data)
     if (code !== ErrorCode.SUCCESS){
       ElNotification({
         title: 'Error',
@@ -103,6 +165,7 @@ onBeforeMount(async () => {
    */
   const { code, data } = await connect((window as any).TOKEN)
   if ( code === ErrorCode.SUCCESS) {
+    currentUserId.value = data?.userId;
     ElNotification({
       title: 'Success',
       message: `Connect success, UserId : ${ data?.userId }`,
@@ -130,25 +193,51 @@ const getConversationListHandle = (count?: number, startTime?: number,) => {
   return getConversationList({ count, startTime });
 }
 
-const selectConversation = async (con: IAReceivedConversation) => {
+/**
+ * 
+ * 选中会话 - 清楚会话未读数 - 发送消息回执
+ */
+const selectConversation = async (con: IAReceivedConversation, index: number) => {
   currentConversation.value = con;
   const conversationType = con.conversationType;
   const targetId = con.targetId;
-  // 多端未读数同步
-  const { code } = await clearMessagesUnreadStatus({ conversationType, targetId });
-  if (code !== ErrorCode.SUCCESS) {
-    return
+
+  if (con.unreadMessageCount) {
+    // 多端未读数同步
+    const { code } = await clearMessagesUnreadStatus({ conversationType, targetId });
+    if (code !== ErrorCode.SUCCESS) {
+      return
+    }
+    sendSyncReadStatusMessage({ conversationType, targetId }, Date.now());
+    sendReadReceiptMessage(targetId, con.latestMessage?.messageUId || '', Date.now())
+    localConversationList.value![index].unreadMessageCount = 0
   }
-  sendSyncReadStatusMessage({ conversationType, targetId }, Date.now())
-  const { code: code1, data } = await getHistoryMessagesHandle({ conversationType, targetId })
+  
+  const { code: code1, data } = await getHistoryMessagesHandle({ conversationType, targetId });
   if (code1 !== ErrorCode.SUCCESS) {
     return
   }
   console.log('localMessageList', data)
   localMessageList.value = data?.list
-  await nextTick()
+  // await nextTick();
   const height = rongMessageListRef.value!.clientHeight - rongMessageBoxRef.value!.clientHeight + 9.5
   scrollbarRef.value?.setScrollTop(height);
+
+  const key = `${(window as any).APPKEY}-${currentUserId.value}-RECEIVED`;
+  const lolRECEIVED = localStorage.getItem(key);
+  if (lolRECEIVED) {
+    currentUnreadInfo.value = JSON.parse(lolRECEIVED!)[`${con.conversationType}-${con.targetId}`] ||  {
+    messageUId: '',
+    sentTime: 0,
+  }
+    return
+  }
+  currentUnreadInfo.value = {
+    messageUId: '',
+    sentTime: 0,
+  }
+
+  console.log('currentUnreadInfo.value', currentUnreadInfo.value)
 }
 
 /**
@@ -162,10 +251,26 @@ const getHistoryMessagesHandle = async (conversation: IConversationOption, optio
  * IMLib 发送消息
  */
 const sendMessageHandle = async () => {
-  const conversationType = currentConversation.value!.conversationType;
-  const targetId = currentConversation.value!.targetId;
+  let conversationType;
+  let targetId;
+  if (activeIndex.value === '1') {
+    conversationType = currentConversation.value!.conversationType;
+    targetId = currentConversation.value!.targetId;
+  } else{
+    conversationType = 6
+    targetId = currentChatroom.value.targetId;
+  }
   const message = new TextMessage({ content: textarea.value });
-  const { code, data } = await sendMessage({ conversationType, targetId }, message);
+  const options = {
+    canIncludeExpansion: true,
+    expansion: {
+      recallMessage: JSON.stringify({
+        status: false,
+        user: currentUserId.value,
+      })
+    }
+  }
+  const { code, data } = await sendMessage({ conversationType, targetId }, message, options);
   if (code !== 0) {
     ElNotification({
       title: 'Error',
@@ -180,6 +285,104 @@ const sendMessageHandle = async () => {
   await nextTick()
   const height = rongMessageListRef.value!.clientHeight - rongMessageBoxRef.value!.clientHeight + 9.5
   scrollbarRef.value?.setScrollTop(height);
+}
+
+/**
+ * IMLib 删除消息
+ */
+
+const deleteMessagesHandle = async (message: IAReceivedMessage, index: number) => {
+  const conversationType = message.conversationType;
+  const targetId = message.targetId;
+  const { code } = await deleteMessages({ conversationType, targetId }, [{
+    messageUId: message.messageUId,
+    sentTime: message.sentTime,
+    messageDirection: message.messageDirection
+  }]);
+  if (code === ErrorCode.SUCCESS) {
+    ElNotification({
+      title: 'Success',
+      message: `Delete success: ${ message.messageUId }`,
+      type: 'success',
+    });
+    localMessageList.value?.splice(index, 1)
+    // localMessageList.value = localMessageList.value?.filter((item) => item.messageUId !== message.messageUId);
+  }
+}
+
+/**
+ * IMLib 撤回消息
+ */
+const recallMessageHandle = async (message: IAReceivedMessage, index: number) => {
+  const { code, data } = await recallMessage({
+    conversationType: message.conversationType,
+    targetId: message.targetId,
+  },message);
+  if (code !== ErrorCode.SUCCESS) {
+    return
+  }
+  ElNotification({
+    title: 'Success',
+    message: `Recall success: ${ message.messageUId }`,
+    type: 'success',
+  });
+  localMessageList.value?.splice(index, 1)
+  localMessageList.value?.push(data!)
+  // localMessageList.value = localMessageList.value?.filter((item) => item.messageUId !== message.messageUId);
+}
+
+/**
+ * IMLib 更新消息扩展
+ */
+const updateMessageExpansionHandle = async (message: IAReceivedMessage, index: number) => {
+  console.log(message)
+  const recallMessage = JSON.stringify({
+    status: true,
+    user: currentUserId.value 
+  })
+  const { code, data } = await updateMessageExpansion({ recallMessage }, message);
+  if (code !== ErrorCode.SUCCESS) {
+    return
+  }
+  localMessageList.value![index].expansion = { recallMessage }
+}
+
+const dropdownActive = ref<boolean>(false)
+const visibleChangeHandle = (data: number) => {
+  console.log('handleOpen', data, rongMessageListRef.value?.getElementsByClassName(`active${data}`)[0])
+  const dropdownDom = rongMessageListRef.value?.getElementsByClassName(`active${data}`)[0]
+  dropdownActive.value = !dropdownActive.value
+  if (!dropdownDom) return
+  if (dropdownActive.value) {
+    dropdownDom!.setAttribute('style', 'display: block')
+    return
+  }
+  dropdownDom!.setAttribute('style', 'display: none')
+  // rongMessageListRef.value?.getElementsByClassName(`active${data}`)[0].setAttribute('style', 'display: block')
+}
+
+/**
+ * IMLib 加入聊天室
+ */
+const joinChatRoomHandle = async (item: { targetId: string, name: string }) => {
+  const { code } = await joinChatRoom(item.targetId, { count: -1 })
+  if (code !== ErrorCode.SUCCESS) {
+    return
+  }
+  ElNotification({
+    title: 'Success',
+    message: `Join success, ChatroomId : ${ item.targetId }`,
+    type: 'success',
+  })
+  const { code: code1, data } = await getChatroomHistoryMessages(item.targetId, {
+    timestamp: 0,
+    count: 20,
+  })
+  if (code1 !== ErrorCode.SUCCESS) {
+    return
+  }
+  currentChatroom.value = item;
+  localMessageList.value = data?.list;
 }
 
 
@@ -206,8 +409,8 @@ const sendMessageHandle = async () => {
     </el-header>
     <el-container class="rong-container-boy">
       <el-aside>
-        <ul class="rong-conversation-list">
-          <li class="rong-conversation" v-for="(item, index) in localConversationList" :key="index" @click="selectConversation(item)">
+        <ul v-if="activeIndex === '1'" class="rong-conversation-list">
+          <li class="rong-conversation" v-for="(item, index) in localConversationList" :key="index" @click="selectConversation(item, index)">
             <el-badge :value="item.unreadMessageCount" :hidden="item.unreadMessageCount === 0" class="item">
               <el-avatar> user </el-avatar>
             </el-badge>
@@ -217,23 +420,52 @@ const sendMessageHandle = async () => {
             </div>
           </li>
         </ul>
+        <ul v-else class="rong-conversation-list">
+          <li class="rong-conversation" v-for="(item, index) in localChatroomList" :key="index" @click="joinChatRoomHandle(item)">
+            <el-avatar> user </el-avatar>
+            <div class="rong-conversation-content">
+              <span>{{ item.targetId }}</span>
+              <p class="rong-conversation-laster-message">{{ item.name }}</p>
+            </div>
+          </li>
+        </ul>
       </el-aside>
       <el-container>
         <el-main style="overflow: hidden;">
           <el-card class="box-card" :body-style="cardCss">
             <template #header>
               <div class="card-header">
-                <span>{{ currentConversation?.targetId || '未选择会话'}}</span>
+                <span v-if="activeIndex === '1'">{{ currentConversation?.targetId || '未选择会话'}}</span>
+                <span v-else>{{ currentChatroom?.targetId || '未选择会话'}}</span>
               </div>
             </template>
             <template #default>
               <div ref="rongMessageBoxRef" style="height: 100%;">
                 <el-scrollbar ref="scrollbarRef" @scroll="scroll">
                   <ul ref="rongMessageListRef" class="rong-message-list">
-                    <li class="rong-message" v-for="(message, index) in localMessageList">
-                      <el-avatar> user </el-avatar>
-                      <div :class="message.messageDirection === 1? 'active' : ''" class="rong-message-content">
-                        <pre>{{ message.content }}</pre>
+                    <li v-for="(message, index) in localMessageList" :key="index">
+                      <el-divider v-if="message.expansion ? JSON.parse(message.expansion!.recallMessage).status : false" content-position="left">{{ JSON.parse(message.expansion!.recallMessage).user }} 撤回了一条消息</el-divider>
+                      <div class="rong-message" v-else>
+                        <el-avatar> user </el-avatar>
+                        <div :class="message.messageDirection === 1? 'active' : ''" class="rong-message-content">
+                          <pre>{{ message.content }}</pre>
+                          
+                        </div>
+                        <div style="position: relative; width: 50px;">
+                          <el-dropdown size="small" @visible-change="visibleChangeHandle(index)" :class="`active${index}`" class="messageHandle" trigger="click">
+                            <el-button size="small" :icon="MoreFilled" />
+                            <template #dropdown>
+                              <el-dropdown-menu>
+                                <el-dropdown-item @click="deleteMessagesHandle(message, index)">删除</el-dropdown-item>
+                                <el-dropdown-item @click="recallMessageHandle(message, index)">撤回</el-dropdown-item>
+                                <el-dropdown-item @click="updateMessageExpansionHandle(message, index)">撤回(扩展)</el-dropdown-item>
+                              </el-dropdown-menu>
+                            </template>
+                          </el-dropdown>
+                          <span class="unread" v-if="activeIndex === '1' && message.messageDirection === 1 && currentUnreadInfo?.sentTime > message.sentTime">已读</span>
+                          <span class="unread" v-if="activeIndex === '1' && message.messageDirection === 1 && currentUnreadInfo?.sentTime < message.sentTime">未读</span>
+                        </div>
+                        
                       </div>
                     </li>
                   </ul>
@@ -316,8 +548,18 @@ const sendMessageHandle = async () => {
 }
 .rong-message {
   display: flex;
-  align-items: start;
   margin-bottom: 10px;
+  align-items: stretch;
+}
+.rong-message-list .messageHandle {
+  display: none;
+  margin-left: 5px;
+}
+.rong-message:hover .messageHandle {
+  display: block !important;
+}
+.rong-message .messageHandle.active {
+  display: block;
 }
 .rong-message-content{
   max-width: 70%;
@@ -338,5 +580,13 @@ const sendMessageHandle = async () => {
   margin: -10px;
   display: flex;
   justify-content: space-between;
+}
+.unread {
+  display: inline-block;
+  position: absolute;
+  bottom: 0;
+  left: 5px;
+  font-size: 12px;
+  width: 50px;
 }
 </style>
